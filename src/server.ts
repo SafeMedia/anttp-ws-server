ab44f465b56548fa1215c4452ecce1ed1c24405f
cobite <chainwizard@proton.me>
2025-05-28 12:22:16 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import type { WebSocket } from "ws";
 import fetch, { Response as FetchResponse } from "node-fetch";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
-const ANTPP_ENDPOINT = "http://localhost:3000/archive";
+const ANTPP_ENDPOINT =
+    process.env.ANTPP_ENDPOINT || "http://localhost:3000/archive";
 const XORNAME_REGEX = /^[a-f0-9]{64}$/i;
 
 type FetchJob = { address: string; ws: WebSocket };
